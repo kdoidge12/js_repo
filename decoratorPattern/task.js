f851958258422abad3d3ac2970ec510453ffcaa8
Doidge <kdoidge@deloitte.com>
2018-03-13 16:51:54 -0400

    Decorator pattern

---
+++ decoratorPattern/task.js
@@ -0,0 +1,36 @@
+var Task = function (name)
+{
+    this.name = name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function(){
+    console.log("completing task: " + this.name);
+    this.completed = true;
+}
+
+Task.prototype.save = function()
+{
+    console.log("saving task: " + this.name);
+}
+
+var myTask = new Task('Legacy Task')
+myTask.complete()
+myTask.save()
+
+var urgentTask = new Task('Urgent Task')
+
+urgentTask.priority = 2;
+urgentTask.notify = function()
+{
+    console.log('notifying important peoples')
+}
+urgentTask.save = function()
+{
+    urgentTask.notify()
+    console.log('saving urgent task!!!')
+    Task.prototype.save.call(this);
+}
+
+urgentTask.save()
+
